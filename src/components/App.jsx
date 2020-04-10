5d414d68c9a9f472be156be53565b603d29786a3
victor tran <victor.tran@tempestas.io>
2020-04-09 22:57:45 -0400

    add mvp + optional features comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import { rooms } from '../data/rooms';
 import { RoomIcon } from './';
 
+/**
+ * * MVP
+ * * see available rooms at a glance,
+ * ! book rooms,
+ * ! add rooms,
+ * ! get room information,
+ * ! checkout guests of room
+ * ! hotel auth - login/register
+ * ! customer auth - login/register
+ *
+ * * Optional Features:
+ * ? payment system
+ *
+ */
+
 function App() {
     const roomIcons = Object.keys(rooms).map((roomId) => {
         const { number, status } = rooms[roomId];
